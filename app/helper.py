a103f6c4383d366ec27ffb5702fbecdb162e9959
Robin Tully <mr.robintully@gmail.com>
2018-08-16 21:36:06 -0400

    really breaking now

--- app/helper.py
+++ app/helper.py
@@ -1,4 +1,4 @@
 from random import randint
 
 def rand_result():
-	return randint(0,1000000)
+	return 'break' #randint(0,1000000)
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'float'
+    return str(rand_result())
 
 @app.route("/string")
 def string():
